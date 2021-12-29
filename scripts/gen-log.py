4eb69f64db7e7f8f458039346107b822e07d0d65
Garrett Anderson <g@devnull.rip>
2021-12-28 17:39:38 -0800

    Vagrant provisioning with ansible

--- scripts/gen-log.py
+++ scripts/gen-log.py
@@ -61,16 +61,16 @@ def main():
   len_non_match_logs_list = len(non_match_logs)
   num_match_logs = random.randint(0, args.max_logs_range)
   for n in range(0, num_match_logs):
-    random_index = random.randint(0, len_non_match_logs_list)
+    random_index = random.randint(0, len_non_match_logs_list - 1)
     random_log = random.choice(match_logs)
     random_latency = random.randint(0, args.max_latency_range)
     generated_log = re.sub(r"took \d+", f"took {random_latency}", random_log)
     generated_logs[random_index] = generated_log
 
   with open(args.dest_log_file, "w") as f:
-    f.write("\n".join(generated_logs))
+    f.write("".join(generated_logs))
 
-  print("Everything ok")
+  print("Log generated")
 
 
 if __name__ == '__main__':
